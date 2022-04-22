466d1abf1aee74e9308034063254495a83f55b52
Lucas Souza <79222458+lucasdev3@users.noreply.github.com>
2022-04-22 00:10:42 -0300

    update
    incrementando functions de validação

--- assets/js/register.js
+++ assets/js/register.js
@@ -19,4 +19,30 @@ $('#cep').focusout(function () {
 });
 
 
+function validaUser() {
+    let user = $('#user').val();
+    
+    if(!user || user.length < 5 || user.length > 12) {
+        return console.log("Usuário inválido");
+    }
+}
+
+function validaPassword() {
+    let password = $('#password').val();
+    
+    if(!password || password.length < 7 || password.length > 12) {
+        return console.log("Senha inválido");
+    }
+}
+
+function validaConfirmacaoPassword() {
+    let password = $('#password').val();
+    let confirmacaoPassword = $('#confirm-password');
+
+    if(password !== confirmacaoPassword) {
+        console.log('As senhas não coincidem');
+    }
+}
+
+validaUser()
 
